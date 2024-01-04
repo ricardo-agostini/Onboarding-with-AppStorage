d33d65a8208ba67ce5804f0198e2fb166969ca84
Ricardo de Aagostini <ricardoagneto@gmail.com>
2024-01-04 12:40:16 -0300

    first commit

---
+++ Onboarding with AppStorege/Onboarding with AppStorege/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Onboarding with AppStorege
+//
+//  Created by Ricardo de Agostini Neto on 04/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Onboarding with AppStorege/Onboarding with AppStorege/Onboarding_with_AppStoregeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_with_AppStoregeApp.swift
+//  Onboarding with AppStorege
+//
+//  Created by Ricardo de Agostini Neto on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_with_AppStoregeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
